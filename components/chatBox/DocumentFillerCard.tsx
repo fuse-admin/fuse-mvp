27fc8c9c27032979dc928bbf2c89b9ba3c3fa56f
Kwabena Andoh-Baidoo <knandohb@gmail.com>
2024-02-01 20:01:31 -0600

    Remove unnecessary text in DocumentFillerCard component

--- components/chatBox/DocumentFillerCard.tsx
+++ components/chatBox/DocumentFillerCard.tsx
@@ -21,18 +21,6 @@ export default function DocumentFillerCard({ openNewDocModal }: { openNewDocModa
                     To fill out a subscription document, custodian document, or W-9, please enter in the chat what you want to 
                     fill out and who for.
                 </p>
-                <p className="p-3 text-justify text-lg">
-                    For example, if you want to fill out a subscription document or W-9 for a client, you would type in the chat:
-                </p>
-                <p className="p-3 text-center text-lg font-bold">
-                    "Fill out the Ironwood sub doc for Sansa Stark" or "Fill out a W-9 for Jon Snow"
-                </p>
-                <p className="p-3 text-justify text-lg">
-                    Provided that the document is in our database, the system will then fill it out.
-                </p>
-                <p className="p-3 text-justify text-lg">
-                    If you want to fill out a new document, please click the button below and the system will walk you through the process!
-                </p>
             </section>
             <section className='flex flex-col justify-center items-center mt-10 gap-5'>
                 <Button
