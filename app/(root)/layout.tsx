a47564183ff797014c22fe0d9619eff2cb01cc23
Kwabena Andoh-Baidoo <knandohb@gmail.com>
2024-02-03 09:56:33 -0600

    Remove unused import statements

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,5 @@
 import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
-import "@uploadthing/react/styles.css";
 
 export default function RootLayout({
   children,
