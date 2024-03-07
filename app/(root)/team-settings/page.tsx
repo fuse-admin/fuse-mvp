b4d80fc47e25baae79c63de62d25230811c28126
Kwabena Andoh-Baidoo <knandohb@gmail.com>
2024-03-06 23:41:12 -0600

    Fix formatting and update loading page

--- app/(root)/team-settings/page.tsx
+++ app/(root)/team-settings/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
-import { OrganizationSwitcher } from "@clerk/nextjs";
+import { OrganizationSwitcher, OrganizationProfile } from "@clerk/nextjs";
+import { Archivo } from 'next/font/google';
+
+const archivo = Archivo({ subsets: ["latin"] })
 
 const TeamSettings = () => {
   return (
-    <OrganizationSwitcher />
+    <main className='flex flex-col justify-center items-center gap-3'>
+      <h1 className={`${archivo.className} text-center text-4xl text-yellow-500 font-bold`}>Team Settings</h1>
+      <div className='flex flex-col justify-center items-center'>
+          <h2 className='font-bold'>Switch Profile</h2>
+          <OrganizationSwitcher />
+      </div>
+        <OrganizationProfile />
+    </main>
   )
 }
 
